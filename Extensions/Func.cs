05f8f142e343a91539679659f15ed32903268626
koropicot <koropicot@gmail.com>
2013-09-08 22:26:39 +0900

    Funt拡張に混在してたタブ文字をスペースに変更

--- Extensions/Func.cs
+++ Extensions/Func.cs
@@ -140,7 +140,7 @@ namespace Extensions
             return (v1,v2,v3,v4,v5,v6,v7,v8,v9,v10,v11,v12,v13,v14,v15,v16) => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11)(v12)(v13)(v14)(v15)(v16);
         }
 
-	     public static Func<Unit, TResult> Tuplize<TResult>(this Func<TResult> func)
+        public static Func<Unit, TResult> Tuplize<TResult>(this Func<TResult> func)
         {
             return _ => func();
         }
@@ -150,7 +150,7 @@ namespace Extensions
             return () => func(Unit.New());
         }
 
-	     public static Func<Tuple<T1>, TResult> Tuplize<T1, TResult>(this Func<T1, TResult> func)
+        public static Func<Tuple<T1>, TResult> Tuplize<T1, TResult>(this Func<T1, TResult> func)
         {
             return tuple => func(tuple.Item1);
         }
@@ -159,7 +159,7 @@ namespace Extensions
         {
             return (v1) => func(Tuple.Create(v1));
         }
-	     public static Func<Tuple<T1,T2>, TResult> Tuplize<T1,T2, TResult>(this Func<T1,T2, TResult> func)
+        public static Func<Tuple<T1,T2>, TResult> Tuplize<T1,T2, TResult>(this Func<T1,T2, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2);
         }
@@ -168,7 +168,7 @@ namespace Extensions
         {
             return (v1,v2) => func(Tuple.Create(v1,v2));
         }
-	     public static Func<Tuple<T1,T2,T3>, TResult> Tuplize<T1,T2,T3, TResult>(this Func<T1,T2,T3, TResult> func)
+        public static Func<Tuple<T1,T2,T3>, TResult> Tuplize<T1,T2,T3, TResult>(this Func<T1,T2,T3, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2,tuple.Item3);
         }
@@ -177,7 +177,7 @@ namespace Extensions
         {
             return (v1,v2,v3) => func(Tuple.Create(v1,v2,v3));
         }
-	     public static Func<Tuple<T1,T2,T3,T4>, TResult> Tuplize<T1,T2,T3,T4, TResult>(this Func<T1,T2,T3,T4, TResult> func)
+        public static Func<Tuple<T1,T2,T3,T4>, TResult> Tuplize<T1,T2,T3,T4, TResult>(this Func<T1,T2,T3,T4, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2,tuple.Item3,tuple.Item4);
         }
@@ -186,7 +186,7 @@ namespace Extensions
         {
             return (v1,v2,v3,v4) => func(Tuple.Create(v1,v2,v3,v4));
         }
-	     public static Func<Tuple<T1,T2,T3,T4,T5>, TResult> Tuplize<T1,T2,T3,T4,T5, TResult>(this Func<T1,T2,T3,T4,T5, TResult> func)
+        public static Func<Tuple<T1,T2,T3,T4,T5>, TResult> Tuplize<T1,T2,T3,T4,T5, TResult>(this Func<T1,T2,T3,T4,T5, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2,tuple.Item3,tuple.Item4,tuple.Item5);
         }
@@ -195,7 +195,7 @@ namespace Extensions
         {
             return (v1,v2,v3,v4,v5) => func(Tuple.Create(v1,v2,v3,v4,v5));
         }
-	     public static Func<Tuple<T1,T2,T3,T4,T5,T6>, TResult> Tuplize<T1,T2,T3,T4,T5,T6, TResult>(this Func<T1,T2,T3,T4,T5,T6, TResult> func)
+        public static Func<Tuple<T1,T2,T3,T4,T5,T6>, TResult> Tuplize<T1,T2,T3,T4,T5,T6, TResult>(this Func<T1,T2,T3,T4,T5,T6, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2,tuple.Item3,tuple.Item4,tuple.Item5,tuple.Item6);
         }
@@ -204,7 +204,7 @@ namespace Extensions
         {
             return (v1,v2,v3,v4,v5,v6) => func(Tuple.Create(v1,v2,v3,v4,v5,v6));
         }
-	     public static Func<Tuple<T1,T2,T3,T4,T5,T6,T7>, TResult> Tuplize<T1,T2,T3,T4,T5,T6,T7, TResult>(this Func<T1,T2,T3,T4,T5,T6,T7, TResult> func)
+        public static Func<Tuple<T1,T2,T3,T4,T5,T6,T7>, TResult> Tuplize<T1,T2,T3,T4,T5,T6,T7, TResult>(this Func<T1,T2,T3,T4,T5,T6,T7, TResult> func)
         {
             return tuple => func(tuple.Item1,tuple.Item2,tuple.Item3,tuple.Item4,tuple.Item5,tuple.Item6,tuple.Item7);
         }
@@ -214,77 +214,77 @@ namespace Extensions
             return (v1,v2,v3,v4,v5,v6,v7) => func(Tuple.Create(v1,v2,v3,v4,v5,v6,v7));
         }
         public static Func<T1, TResult> _<T1, TResult>(
-		    this Func<T1, TResult> func)
+            this Func<T1, TResult> func)
         {
             return v1 => func(v1);
         }
         public static Func<T2, Func<T1, TResult>> _<T1,T2, TResult>(
-		    this Func<T1, Func<T2, TResult>> func)
+            this Func<T1, Func<T2, TResult>> func)
         {
             return v2 => v1 => func(v1)(v2);
         }
         public static Func<T2, Func<T3, Func<T1, TResult>>> _<T1,T2,T3, TResult>(
-		    this Func<T1, Func<T2, Func<T3, TResult>>> func)
+            this Func<T1, Func<T2, Func<T3, TResult>>> func)
         {
             return v2 => v3 => v1 => func(v1)(v2)(v3);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T1, TResult>>>> _<T1,T2,T3,T4, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, TResult>>>> func)
         {
             return v2 => v3 => v4 => v1 => func(v1)(v2)(v3)(v4);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T1, TResult>>>>> _<T1,T2,T3,T4,T5, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, TResult>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v1 => func(v1)(v2)(v3)(v4)(v5);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T1, TResult>>>>>> _<T1,T2,T3,T4,T5,T6, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, TResult>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T1, TResult>>>>>>> _<T1,T2,T3,T4,T5,T6,T7, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, TResult>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T1, TResult>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, TResult>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T1, TResult>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, TResult>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, TResult>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T1, TResult>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, TResult>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, TResult>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T1, TResult>>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, TResult>>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, TResult>>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v11 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T1, TResult>>>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, TResult>>>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, TResult>>>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v11 => v12 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11)(v12);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T1, TResult>>>>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, TResult>>>>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, TResult>>>>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v11 => v12 => v13 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11)(v12)(v13);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T1, TResult>>>>>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, TResult>>>>>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, TResult>>>>>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v11 => v12 => v13 => v14 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11)(v12)(v13)(v14);
         }
         public static Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, Func<T1, TResult>>>>>>>>>>>>>>> _<T1,T2,T3,T4,T5,T6,T7,T8,T9,T10,T11,T12,T13,T14,T15, TResult>(
-		    this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, TResult>>>>>>>>>>>>>>> func)
+            this Func<T1, Func<T2, Func<T3, Func<T4, Func<T5, Func<T6, Func<T7, Func<T8, Func<T9, Func<T10, Func<T11, Func<T12, Func<T13, Func<T14, Func<T15, TResult>>>>>>>>>>>>>>> func)
         {
             return v2 => v3 => v4 => v5 => v6 => v7 => v8 => v9 => v10 => v11 => v12 => v13 => v14 => v15 => v1 => func(v1)(v2)(v3)(v4)(v5)(v6)(v7)(v8)(v9)(v10)(v11)(v12)(v13)(v14)(v15);
         }
